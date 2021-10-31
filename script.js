06ddc08bef9eec4bb9b8fe0ca2184cca765a5d2b
amadeamargo <amadeamargo@gmail.com>
2021-10-31 02:04:38 -0500

    input form added

--- script.js
+++ script.js
@@ -1,20 +1,79 @@
-/* add important events to daily planner 
-
-open planner
-current day is diplayed at the top of the calendar
-
-scroll down --> timeblocks for standard businees hours (9-5)
-when view time blocks for that day
-time block is color coded to indicate whether it is in the past, present or future
-
+/* 
+display current day at the top of calendar
+    - use moment js 
+timeblocks for standard business hours (9-5)
+    - use bootstrap to create rows
+color code time block to see whether it is in the past, present or future
+    - use moment js to change background color of timeblocks
+    - green for current time block
+    - grey for past time block
+    - white for future time block
 click on time block to enter event
+    - add event listener click on timeblock
+    - allow user to input data
+        #event title
 click save button then text for that event is saved in local storage
-
+    - add event listner to saveBtn
+    - create function to save to local storage
 when refresh page, the saved event persists
-
-
-
 */
-var currentTime = moment().format("LLLL");
-$("#currentDay").text(currentTime);
+
+
+//display current day on top of calendar
+var currentDay = moment().format("LLLL");
+$("#currentDay").text(currentDay);
+
+//user can input event information upon clicking time block
+var timeBlock = document.getElementById("time-block")
+timeBlock.addEventListener('click', displayInputBox)
+
+
+function displayInputBox(event){
+    console.log("ello");
+    event.preventDefault();
+
+    var inputBoxContainer = document.createElement('div')
+    var inputBox = document.createElement('input')
+    inputBoxContainer
+    inputBox.innerHTML = "<input type='text' id='newInputBox'>";
+    document.getElementById("time-block").appendChild(inputBoxContainer);
+    document.getElementById("time-block").appendChild(inputBox);
+    var inputDetails = inputBox.value
+    var eventEl = document.createElement('p')
+    eventEl = inputDetails
+    
+    printEvent()
+    
+}
+
+// print event onto window
+var saveBtn = document.getElementById('#saveBtn')
+
+function printEvent(){
+    console.log("saved")
+}
+// saveBtn.addEventListener('click', printEvent)
+
+
+//event gets saved into local storage
+
+// var save = document.getElementById("saveBtn")
+// save.addEventListener('click', saveEvent)
+
+// function saveEvent(){
+//     console.log("save")
+// }
+
+
+
+// var nowTimeblock = moment()
+
+// function colorCode (){
+//     var currentTime = moment()
+//     if currentTime <9 && >5
+//     }
+
+// }
+
+
 
